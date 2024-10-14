0e5f77e40c3115daddb2825e624d854c6786fa77
Berto <albertoredondo@paucasesnovescifp.cat>
2024-10-14 13:44:08 +0200

    Primer ejercicio del 4
    Creada la primera clase

---
+++ EjercicioMultiprocesos4/src/ExecutaDir.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ExecutaDir {
+    public static void main(String[] args) {
+
+        Path dir = Path.of(System.getProperty("user.home"), "Desktop");
+        try (BufferedWriter writer = new BufferedWriter(new PrintWriter(new File("archivo.txt")))) {
+            muestraDirectorio(dir, 0, writer);
+            System.out.println("Archivo creado");
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
+
+    }
+    
+    public static void muestraDirectorio(Path dir, int sangrado, BufferedWriter writer) throws IOException {
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+
+            for (Path element : stream) {
+
+                    writer.write(" ".repeat(sangrado) + element.getFileName() + "\n");
+                    writer.flush();
+
+            }
+            stream.close();
+        }
+        catch (IOException ioe) {
+            System.err.println("Algo ha sido un verdadero desastre");
+        }
+    }
+}
